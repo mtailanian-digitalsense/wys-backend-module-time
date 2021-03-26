38604f9be41dd47369c80a5eda362f9e58b93663
aquiles <aquiles.viza@sansano.usm.cl>
2021-03-26 13:10:14 -0300

    Agregada las columnas, además de variables de entorno de Buildings

--- main.py
+++ main.py
@@ -23,12 +23,20 @@ DB_PASS = os.getenv('DB_PASSWORD', 'rac3e/07')
 DB_IP = os.getenv('DB_IP_ADDRESS', '10.2.19.195')
 DB_PORT = os.getenv('DB_PORT', '3307')
 DB_SCHEMA = os.getenv('DB_SCHEMA', 'wys')
+
 APP_HOST = os.getenv('APP_HOST', '127.0.0.1')
 APP_PORT = os.getenv('APP_PORT', 5007)
+
 PROJECTS_MODULE_HOST = os.getenv('PROJECTS_MODULE_HOST', '127.0.0.1')
 PROJECTS_MODULE_PORT = os.getenv('PROJECTS_MODULE_PORT', 5000)
 PROJECTS_MODULE_API = os.getenv('PROJECTS_MODULE_API', '/api/projects/')
 PROJECTS_URL = f"http://{PROJECTS_MODULE_HOST}:{PROJECTS_MODULE_PORT}"
+
+BUILDINGS_MODULE_HOST = os.getenv('BUILDINGS_MODULE_HOST', '127.0.0.1')
+BUILDINGS_MODULE_PORT = os.getenv('BUILDINGS_MODULE_PORT', 5004)
+BUILDINGS_MODULE_API = os.getenv('BUILDINGS_MODULE_API', '/api/buildings/')
+BUILDINGS_URL = f"http://{BUILDINGS_MODULE_HOST}:{BUILDINGS_MODULE_PORT}"
+
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{DB_USER}:{DB_PASS}@{DB_IP}:{DB_PORT}/{DB_SCHEMA}"
@@ -74,9 +82,9 @@ class TimeGen(db.Model):
     "m2": 569.0
     """
     id = db.Column(db.Integer, primary_key=True)
-    adm_agility = db.Column(db.String(45))
+    adm_agility = db.Column(db.String(45), nullable=True) # No hay que ocuparlo
     client_agility = db.Column(db.String(45))
-    mun_agility = db.Column(db.String(45))
+    mun_agility = db.Column(db.String(45), nullable=True) # No hay que ocuparlo
     construction_mod = db.Column(db.String(45))
     constructions_times = db.Column(db.String(45))
     procurement_process = db.Column(db.String(45))
